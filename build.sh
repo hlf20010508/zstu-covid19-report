d7574cacd804419780c3bf232e5d1dafa441e09d
hlf20010508 <hlf01@icloud.com>
2022-08-01 16:59:14 +0800

    保存用户信息

--- build.sh
+++ build.sh
@@ -4,7 +4,11 @@ sudo docker build -f ./Dockerfile -t zstu-covid19-report .
 sudo docker rmi $(sudo docker images -f "dangling=true" -q)
 sudo docker rmi hlf01/python-selenium
 echo "镜像创建完成"
+if [ ! -f "user.conf" ]; then
 read -p "请输入你的学号：" ZSTU_ID
 read -p "请输入密码：" ZSTU_PASSWD
+echo $ZSTU_ID > user.conf
+echo $ZSTU_PASSWD >> user.conf
+fi
 echo "尝试创建容器并第一次运行"
 sudo docker run --name zstu-covid19-report -e ZSTU_ID=$ZSTU_ID -e ZSTU_PASSWD=$ZSTU_PASSWD -i zstu-covid19-report bash -c "cd /srv/zstu && python main.py"
