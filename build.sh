9629f027eede6e56b3d3d8d578b5881a384677ec
hlf20010508 <hlf01@icloud.com>
2022-08-01 16:41:46 +0800

    自动删除过期镜像和容器

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #! /bin/bash
 
 sudo docker build -f ./Dockerfile -t zstu-covid19-report .
+sudo docker rmi $(sudo docker images -f "dangling=true" -q)
+sudo docker rmi hlf01/python-selenium
 echo "镜像创建完成，尝试第一次运行"
 sudo docker run --name zstu-covid19-report -e ZSTU_ID=$ZSTU_ID -e ZSTU_PASSWD=$ZSTU_PASSWD -i zstu-covid19-report bash -c "cd /srv/zstu && python main.py"
--- update.sh
+++ update.sh
@@ -3,5 +3,5 @@
 echo "删除过期容器"
 sudo docker rm zstu-covid19-report
 echo "删除过期镜像"
-sudo docker images prune -a -f
+sudo docker rmi zstu-covid19-report
 bash build.sh
